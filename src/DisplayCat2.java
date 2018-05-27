5e5eea4367963b7ebb3fd6f66bee938edf191916
Alice <alice.stelmach@gmail.com>
2018-05-27 10:55:59 +0200

    Stworzenie projektu-dodanie klasy

---
+++ src/DisplayCat2.java
@@ -0,0 +1,7 @@
+public class DisplayCat2 {
+
+    public static void main(String[] args) {
+        String s = Character.toString((char)75)+Character.toString((char)79)+Character.toString((char)84);
+        System.out.println((s));
+    }
+}
